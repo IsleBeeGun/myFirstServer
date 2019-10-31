2a3f7ff19fda1d896ac9f6133eeee57e67fd8752
IsleBeeGun <IsleBeeGun@gmail.com>
2019-10-31 19:49:08 +0300

    Test ports 4001 & 3000, current 4001

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 let { requestListener } = require('./callbackFile.js');
 const PORT = process.env.PORT || 4001;
+//const PORT = 3000;
 
 const server = http.createServer(requestListener);
 server.listen(PORT);
